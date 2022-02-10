7676f02714c6a242233b9b22a639bb9f6c76f055
nirzaf <mfmfazrin1986@gmail.com>
2022-02-10 16:18:23 +0530

    sum += int.Parse(temp);                 temp = "0";

--- Program.cs
+++ Program.cs
@@ -1,45 +1,28 @@
-﻿using static System.Console;
+﻿using static System.Char;
+using static System.Console;
 
 var arr = Find.Numbers("88Hello 3World!");
 WriteLine(arr);
 
 
 
-public static class Find{
+public static class Find
+{
+    public static int Numbers(string str)
+    {
+       string temp = "0";
 
-//find all numbers and add into array from a string input
-   public static int Numbers(string str)
-   {
-        // A temporary string
-        string temp = "0";
- 
-        // holds sum of all numbers
-        // present in the string
         int sum = 0;
- 
-        // read each character in input string
-        for (int i = 0; i < str.Length; i++) {
-            char ch = str[i];
- 
-            // if current character is a digit
-            if (char.IsDigit(ch))
+
+        foreach (char ch in str)
+        {
+            if (IsDigit(ch))
                 temp += ch;
- 
-            // if current character is an alphabet
             else {
- 
-                // increment sum by number found earlier
-                // (if any)
                 sum += int.Parse(temp);
- 
-                // reset temporary string to empty
                 temp = "0";
             }
         }
- 
-        // atoi(temp.c_str()) takes care of trailing
-        // numbers
         return sum + int.Parse(temp);
     }
-
 }
